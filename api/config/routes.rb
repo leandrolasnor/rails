bbb04f86a8673a4342390c3db3749b7f11b74722
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2022-08-06 17:21:18 -0300

    chore: config namespace moat on controllers

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,18 +1,20 @@
+# frozen_string_literal: true
+
 require 'sidekiq/web'
 
 # Configure Sidekiq-specific session middleware
 Sidekiq::Web.use ActionDispatch::Cookies
-Sidekiq::Web.use ActionDispatch::Session::CookieStore, key: "_interslice_session"
+Sidekiq::Web.use ActionDispatch::Session::CookieStore, key: '_interslice_session'
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
-  mount_devise_token_auth_for 'User', at: 'auth', controllers:{sessions: 'overrides/sessions', registrations: 'overrides/registrations'}
+  mount_devise_token_auth_for 'User', at: 'auth', controllers: { sessions: 'overrides/sessions', registrations: 'overrides/registrations' }
+  root to: 'api#health', via: :all
+  match '*path' => 'api#not_found', via: :all
 
-  root to: "api#health", via: :all
-  
-  get  '/albums/search/:query', query: /[a-zA-Z0-9\-_]+/, to: 'albums#search'
-  get  '/albums/search', to: 'albums#search'
-  resources :albums, id: /[0-9\-_]+/, only: [:show, :update, :destroy, :create]
-
-  get '/artists', to: 'artists#list'
-  match "*path" => "api#not_found", via: :all
+  namespace :moat do
+    get  '/albums/search/:query', query: /[a-zA-Z0-9\-_]+/, to: 'albums#search'
+    get  '/albums/search', to: 'albums#search'
+    resources :albums, id: /[0-9\-_]+/, only: [:show, :update, :destroy, :create]
+    get '/artists', to: 'artists#list'
+  end
 end
