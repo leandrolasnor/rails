c023d452b616fc5e345b9c5d68cfa04a58ed02ad
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2022-08-06 18:47:52 -0300

    chore: rubocop

--- api/config/application.rb
+++ api/config/application.rb
@@ -1,15 +1,17 @@
+# frozen_string_literal: true
+
 require_relative 'boot'
 
-require "rails"
+require 'rails'
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_cable/engine"
-require "active_job/railtie"
+require 'active_model/railtie'
+require 'active_record/railtie'
+require 'action_controller/railtie'
+require 'action_cable/engine'
+require 'active_job/railtie'
 # require "active_storage/engine"
 # require "action_text/engine"
-require "action_mailer/railtie"
+require 'action_mailer/railtie'
 # require "action_mailbox/engine"
 # require "action_view/railtie"
 # require "sprockets/railtie"
@@ -22,7 +24,7 @@ Bundler.require(*Rails.groups)
 module RailsApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults(6.0)
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
--- api/spec/support/auth/auth_helper.rb
+++ api/spec/support/auth/auth_helper.rb
@@ -1,27 +1,29 @@
+# frozen_string_literal: true
+
 module Auth
-  SIGN_IN_HEADER = {"Content-Type": "application/json"}
+  SIGN_IN_HEADER = { 'Content-Type': 'application/json' }.freeze
   def sign_in(user: nil)
     create_users_default if User.count.zero?
-    user = {email: 'teste@teste.com', password: '123456'} if user.nil?
-    post user_session_path, params: {:email => user[:email], :password => user[:password]}.to_json, headers: SIGN_IN_HEADER
-    {body: JSON.parse(response.body, symbolize_names: true), headers: response.headers }
+    user = { email: 'teste@teste.com', password: '123456' } if user.nil?
+    post(user_session_path, params: { email: user[:email], password: user[:password] }.to_json, headers: SIGN_IN_HEADER)
+    { body: JSON.parse(response.body, symbolize_names: true), headers: response.headers }
   end
 
   def create_users_default
     User.create!(
-      name:                   "Teste",
-      email:                  "teste@teste.com",
-      password:               "123456",
-      password_confirmation:  "123456",
-      role:                   0
+      name: 'Teste',
+      email: 'teste@teste.com',
+      password: '123456',
+      password_confirmation: '123456',
+      role: 0
     )
-    
+
     User.create!(
-      name:                   "Other Teste",
-      email:                  "otherteste@teste.com",
-      password:               "123456",
-      password_confirmation:  "123456",
-      role:                   1
+      name: 'Other Teste',
+      email: 'otherteste@teste.com',
+      password: '123456',
+      password_confirmation: '123456',
+      role: 1
     )
   end
 end
