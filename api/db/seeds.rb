3b0f89156151c2e4707d5074c73a85947cb8f46d
Leandro Lasnor <leandro.lasnor@mesainc.com.br>
2022-08-07 03:44:15 -0300

    chore: rubocop

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -7,25 +9,25 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.create!(
-  name:                   "Teste",
-  email:                  "teste@teste.com",
-  password:               "123456",
-  password_confirmation:  "123456",
+  name:                   'Teste',
+  email:                  'teste@teste.com',
+  password:               '123456',
+  password_confirmation:  '123456',
   role:                   0
 )
 
 User.create!(
-  name:                   "Other Teste",
-  email:                  "otherteste@teste.com",
-  password:               "123456",
-  password_confirmation:  "123456",
+  name:                   'Other Teste',
+  email:                  'otherteste@teste.com',
+  password:               '123456',
+  password_confirmation:  '123456',
   role:                   1
 )
 
 30.times{ 
   Album.create!(
     name: Faker::Games::Pokemon.name,
-    year: rand(1948..Time.now.year).to_i,
-    artist_id: [1,2,3,4,5].sample
+    year: rand(1948..Time.zone.now.year).to_i,
+    artist_id: [1, 2, 3, 4, 5].sample
   )
 }
--- api/spec/support/service/service_helper.rb
+++ api/spec/support/service/service_helper.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: true
+
 module Service
   def successful_body_content
-    "{\"code\":0,\"message\":\"ok\"}"
+    { code: 0, message: 'ok' }.to_json
   end
 
   def successful_response
-    {:content=>{:code=>0, :message=>"ok"}, :status=>:ok}
+    { content: { code: 0, message: 'ok' }, status: :ok }
   end
 
   def unsuccessful_response
-    { content: { code: -1, message: 'failure' }, status: 	:internal_server_error }
+    { content: { code: -1, message: 'failure' }, status: :internal_server_error }
   end
 end
