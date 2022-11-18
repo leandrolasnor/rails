068c54a5580b032f4a465cdbae54dc8240832696
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2022-11-18 00:04:46 -0300

    feat: new service for republic

---
+++ api/app/services/republic/calculate_quantity_of_can_of_paint_service.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module ::Republic
+  class CalculateQuantityOfCanOfPaintService < ApplicationService
+    def call
+      area = CalculatePaintableArea.from(params)
+      handle_response[:content][:payload] = cached_can_of_paint(area)
+      handle_response
+    rescue KeyError, ConfigInvalid => error
+      error_response[:content][:message] = error.message
+      error_response[:status] = :bad_request
+      error_response
+    end
+
+    private
+
+    def cached_can_of_paint(area)
+      Rails.cache.fetch("#{__method__}/#{area}", expire_in: 1.hour, skip_nil: true) do
+        CanOfPaint.for_paint(area)
+      end
+    end
+  end
+end
