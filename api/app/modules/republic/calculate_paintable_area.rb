c3eafe71c3ec41fe0655f70d7ed3ed2fedd474e3
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2022-11-18 00:07:22 -0300

    feat: new modules for republic

---
+++ api/app/modules/republic/calculate_paintable_area.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module ::Republic
+  module CalculatePaintableArea
+    class << self
+      def from(params)
+        paintable = Paintable.new(params)
+        raise ConfigInvalid.new(paintable.errors.full_messages.flatten) if paintable.invalid?
+
+        paintable.area
+      end
+    end
+  end
+end
---
+++ api/app/modules/republic/can_of_paint.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module ::Republic
+  module CanOfPaint
+    prepend Line::Awesome
+    class << self
+      def for_paint(area)
+        area = 0 if area.negative?
+        liters = area.to_f / CanOfPaint::INK_LITER_YIELD
+        necessary_material = Array.new(CanOfPaint::KINDS.count)
+        calculate_material(necessary_material, liters)
+      end
+
+      def calculate_material(necessary_material, liters, index = 0)
+        cop = CanOfPaint::KINDS[index]
+        quantity, liters = liters.divmod(cop)
+        necessary_material[index] = { cop: cop, quantity: quantity }
+        if necessary_material.last.nil?
+          necessary_material = calculate_material(necessary_material, liters, index + 1)
+        elsif liters.positive?
+          necessary_material.last[:quantity] += 1
+        end
+        if index.positive? && necessary_material[index][:quantity] * necessary_material[index][:cop] == necessary_material[index - 1][:cop]
+          necessary_material[index][:quantity] = 0
+          necessary_material[index - 1][:quantity] += 1
+        end
+        necessary_material
+      end
+    end
+  end
+end
